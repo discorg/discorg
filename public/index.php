1aee465fd85beeb5b8b89e67b66f653483d06393
Ondřej Bouda <ondrej.bouda@gmail.com>
2019-05-27 10:39:53 +0200

    move http logic to http application

--- public/index.php
+++ public/index.php
@@ -3,14 +3,11 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Bouda\SpotifyAlbumTagger\Application\HttpApplication;
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Nyholm\Psr7Server\ServerRequestCreator;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Dotenv\Dotenv;
 use Tracy\Debugger;
-use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 const ROOT_DIRECTORY = __DIR__ . '/../';
 
@@ -30,27 +27,6 @@ $loader->load(ROOT_DIRECTORY . 'src/Application/config.yaml');
 $resolveEnvPlaceholders = true;
 $container->compile($resolveEnvPlaceholders);
 
-$psr17Factory = new Psr17Factory();
-
-$creator = new ServerRequestCreator(
-    $psr17Factory,
-    $psr17Factory,
-    $psr17Factory,
-    $psr17Factory
-);
-
-$request = $creator->fromGlobals();
-
-// fix Nyholm\Psr7Server\ServerRequestCreator bug with duplicated port in URI
-$uri = $request->getUri();
-$host = $uri->getHost();
-// remove port from host
-$host = preg_replace('#:[0-9]+$#', '', $host);
-$uri = $uri->withHost($host);
-$request = $request->withUri($uri);
-
 /** @var HttpApplication $application */
 $application = $container->get(HttpApplication::class);
-$response = $application->run($request, $container);
-
-(new SapiEmitter())->emit($response);
+$application->run($container);
--- src/Application/HttpApplication.php
+++ src/Application/HttpApplication.php
@@ -7,9 +7,12 @@ namespace Bouda\SpotifyAlbumTagger\Application;
 use Bouda\SpotifyAlbumTagger\Spotify\Session\InitializableSpotifySessionManager;
 use Bouda\SpotifyAlbumTagger\User\InitializableUserSessionManager;
 use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
+use function preg_replace;
 
 class HttpApplication
 {
@@ -32,9 +35,36 @@ class HttpApplication
         $this->actionResolver = $actionResolver;
     }
 
-    public function run(ServerRequestInterface $request, ContainerInterface $container) : ResponseInterface
+    public function run(ContainerInterface $container) : void
     {
         $psr17Factory = new Psr17Factory();
+
+        $creator = new ServerRequestCreator(
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory
+        );
+
+        $request = $creator->fromGlobals();
+
+        // fix Nyholm\Psr7Server\ServerRequestCreator bug with duplicated port in URI
+        $uri = $request->getUri();
+        $host = $uri->getHost();
+        // remove port from host
+        $host = preg_replace('#:[0-9]+$#', '', $host);
+        $uri = $uri->withHost($host);
+        $request = $request->withUri($uri);
+
+        $response = $this->processRequest($container, $request);
+
+        (new SapiEmitter())->emit($response);
+    }
+
+    public function processRequest(ContainerInterface $container, ServerRequestInterface $request) : ResponseInterface
+    {
+        $psr17Factory = new Psr17Factory();
+
         $response = $psr17Factory->createResponse();
 
         $response = $this->userSessionManager->initialize($request, $response);
@@ -47,6 +77,8 @@ class HttpApplication
 
         $action = $this->actionResolver->resolve($request, $container);
 
-        return $action($request, $response);
+        $response = $action($request, $response);
+
+        return $response;
     }
 }
