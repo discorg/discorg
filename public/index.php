6f715ea56056345f98442ce65b3aae0c8b855d59
Ondřej Bouda <ondrej.bouda@gmail.com>
2019-05-29 15:09:09 +0200

    extract environment variable loading from container factory

--- public/index.php
+++ public/index.php
@@ -6,10 +6,17 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Bouda\SpotifyAlbumTagger\Application\ContainerFactory;
 use Bouda\SpotifyAlbumTagger\Application\HttpApplication;
+use Symfony\Component\Dotenv\Dotenv;
 use Tracy\Debugger;
 
 Debugger::enable();
 Debugger::$maxDepth = 7;
+
+$envFile = __DIR__ . '/../config/.env';
+if (file_exists($envFile)) {
+    $dotenv = new Dotenv();
+    $dotenv->load($envFile);
+}
 
 $containerFactory = new ContainerFactory();
 $container = $containerFactory->create(__DIR__ . '/../');
--- src/Application/ContainerFactory.php
+++ src/Application/ContainerFactory.php
@@ -8,19 +8,11 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Dotenv\Dotenv;
-use function file_exists;
 
 final class ContainerFactory
 {
     public function create(string $rootDirectory) : Container
     {
-        $envFile = $rootDirectory . 'config/.env';
-        if (file_exists($envFile)) {
-            $dotenv = new Dotenv();
-            $dotenv->load($envFile);
-        }
-
         $container = new ContainerBuilder();
         $loader = new YamlFileLoader($container, new FileLocator($rootDirectory));
         $loader->load($rootDirectory . 'src/Application/config.yaml');
