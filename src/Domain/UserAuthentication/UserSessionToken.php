f203f3480f65322a82bad47a1c7998644f50093d
Ondřej Bouda <ondrej.bouda@gmail.com>
2020-03-23 21:46:13 +0100

    use uuid for user session token

--- src/Domain/UserAuthentication/UserSessionToken.php
+++ src/Domain/UserAuthentication/UserSessionToken.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Domain\UserAuthentication;
 
+use Ramsey\Uuid\Uuid;
+
 final class UserSessionToken
 {
     private string $token;
@@ -15,7 +17,7 @@ final class UserSessionToken
 
     public static function generate() : self
     {
-        return new self('12345');
+        return new self(Uuid::uuid4()->toString());
     }
 
     public static function fromStoredValue(string $token) : self
@@ -26,5 +28,10 @@ final class UserSessionToken
     public function toString() : string
     {
         return $this->token;
+    }
+
+    public function equals(UserSessionToken $token) : bool
+    {
+        return $this->token === $token->token;
     }
 }
---
+++ tests/Unit/Domain/UserAuthentication/UserSessionTokenTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain\UserAuthentication;
+
+use App\Domain\UserAuthentication\UserSessionToken;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use function strlen;
+
+class UserSessionTokenTest extends TestCase
+{
+    public function testGeneratedTokensAreDifferent() : void
+    {
+        $token1 = UserSessionToken::generate();
+        $token2 = UserSessionToken::generate();
+
+        Assert::assertFalse($token1->equals($token2));
+    }
+
+    public function testGeneratedTokenIsComplexEnough() : void
+    {
+        $token = UserSessionToken::generate();
+
+        Assert::assertTrue(strlen($token->toString()) > 20);
+    }
+
+    public function testFromStoredValue() : void
+    {
+        $token = UserSessionToken::fromStoredValue('test');
+
+        Assert::assertSame('test', $token->toString());
+    }
+}
